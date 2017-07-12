b42b19b4ce07d15c0a13f802f679f57072b5592f
prushly <purushothptm@gmail.com>
2017-07-12 11:28:00 +0530

    Add files via upload

---
+++ hello2.java
@@ -0,0 +1,13 @@
+public class Ideone {
+
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		
+		
+	for(int a=1;a<=5;a++){
+		System.out.println("Hello");
+		
+	}}
+
+}
