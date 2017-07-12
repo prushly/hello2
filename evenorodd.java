2fe76dfe52b47df36ffaab843378e8032e023324
prushly <purushothptm@gmail.com>
2017-07-12 23:07:41 +0530

    Add files via upload

---
+++ evenorodd.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Ideone {
+
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		
+		int a;
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the number:");
+		
+		a=s.nextInt();
+		if(a%2  == 0 ){
+			System.out.println("It is an even number");
+		}
+		else{
+			System.out.println("It is an odd numer");
+		
+		}
+		
+		
+		}
+	}
